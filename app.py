ddc6c7b18b60d6b2b8a87e6cdc3eb9cc83cc0c08
aapodacah <aapodaca@palo-it.com>
2026-04-12 20:19:06 -0300

    prueba inicial

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
     return render_template(
@@ -11,6 +10,5 @@ def index():
         mensaje="Esta es la página principal con texto e imagen.",
     )
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
